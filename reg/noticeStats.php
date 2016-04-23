2a7ce5d0d59316f179013f95d48cf28666a46685
summercyy <summercyy@pku.edu.cn>
2016-04-23 12:33:14 +0800

    函数包装

--- reg/noticeStats.php
+++ reg/noticeStats.php
@@ -8,46 +8,47 @@
  * Time: 21:13
  */
 
-require_once "../dataBaseApi/dataBaseApis.php";
+function stat()
+{
+    require_once "../dataBaseApi/dataBaseApis.php";
 
-/*foreach ($_REQUEST as $key => $value ){
-    echo "key: " . $key;
-    echo "   value: " . $value;
-}*/
+    /*foreach ($_REQUEST as $key => $value ){
+        echo "key: " . $key;
+        echo "   value: " . $value;
+    }*/
 
-$questionnaireID = $_REQUEST["questionnaireID"];
+    $questionnaireID = $_REQUEST["questionnaireID"];
 
-$selected = getPeopleSelected($questionnaireID);
-$selectedNum = 0;
-$notSelected = getPeopleNotSelected($questionnaireID);
-$notSelectedNum = 0;
-$s=array();
-$ns=array();
-if ($selected->num_rows > 0) {
-    while ($row = $selected->fetch_assoc()) {
-        //echo "studentName: {$row["studentName"]}<br>";
-        array_push($s,$row["studentName"]);
-        $selectedNum++;
+    $selected = getPeopleSelected($questionnaireID);
+    $selectedNum = 0;
+    $notSelected = getPeopleNotSelected($questionnaireID);
+    $notSelectedNum = 0;
+    $s = array();
+    $ns = array();
+    if ($selected->num_rows > 0) {
+        while ($row = $selected->fetch_assoc()) {
+            //echo "studentName: {$row["studentName"]}<br>";
+            array_push($s, $row["studentName"]);
+            $selectedNum++;
+        }
+        //echo "{$selectedNum} students have read this notice.<br>";
+    } else {
+        //echo "No student has read this notice.<br>";
     }
-    //echo "{$selectedNum} students have read this notice.<br>";
-}
-else {
-    //echo "No student has read this notice.<br>";
-}
 
-if ($notSelected->num_rows > 0) {
-    while ($row = $notSelected->fetch_assoc()) {
-        //echo "studentName: {$row["studentName"]}<br>" ;
-        array_push($ns,$row["studentName"]);
-        $notSelectedNum++;
+    if ($notSelected->num_rows > 0) {
+        while ($row = $notSelected->fetch_assoc()) {
+            //echo "studentName: {$row["studentName"]}<br>" ;
+            array_push($ns, $row["studentName"]);
+            $notSelectedNum++;
+        }
+        //echo "{$notSelectedNum} students haven't read this notice.<br>";
+    } else {
+        //echo "No student hasn't read this questionnaire<br>.";
     }
-    //echo "{$notSelectedNum} students haven't read this notice.<br>";
-}
-else {
-    //echo "No student hasn't read this questionnaire<br>.";
-}
 
-$arr = array('s'=>$s, 'ns'=>$ns);
-$jsonencode = json_encode($arr);
+    $arr = array('s' => $s, 'ns' => $ns);
+    $jsonencode = json_encode($arr);
 //echo "$jsonencode";
-return $jsonencode;
+    return $jsonencode;
+}
