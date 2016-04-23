1ceae70da5c1baf3aab36939e1014aa144288ef6
wtsun-pku <1200012617@pku.edu.cn>
2016-04-23 22:01:39 +0800

    teacher
    sdfqsd

--- reg/teacherReg.php
+++ reg/teacherReg.php
@@ -61,8 +61,8 @@ $userRegNewUrlOAuth = "https://open.weixin.qq.com/connect/oauth2/authorize?appid
 //echo "<button class=\"btn btn-large btn-block\" type=\"button\">复制链接</button>"."<br>";
 ?>
 -->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html >
+<html >
 <head>
     <title>创建通知群</title>
     <meta http-equiv="Content-Type" name="viewport" content="width=device-width, initial-scale=1.0; charset=utf-8">
@@ -70,44 +70,29 @@ $userRegNewUrlOAuth = "https://open.weixin.qq.com/connect/oauth2/authorize?appid
     <script src="../pages/reference/jquery.min.js"></script>
     <script src="../pages/reference/bootstrap.min.js"></script>
     <script src="../pages/reference/bootstrap-theme.min.css"></script>
-    <script type="text/javascript" src="../pages/reference/ZeroClipboard.js"></script>
-    <script type="text/javaScript">
-        var clip = null;
-        function $(id) { return document.getElementById(id); }
-        function init() {
-            clip = new ZeroClipboard.Client();
-            clip.setHandCursor(true);
-            clip.addEventListener('mouseOver', function (client) {
-                // update the text on mouse over
-                clip.setText( $('fe_text').value );
-            });
-
-            clip.addEventListener('complete', function (client, text) {
-                //debugstr("Copied text to clipboard: " + text );
-                alert("该地址已经复制，你可以使用Ctrl+V 粘贴。");
-            });
-
-            clip.glue('clip_button', 'clip_container' );
-        }
-    </script>
 </head>
-<body onLoad="init()">
-<input id="fe_text" cols="50" rows="5" value="复制内容文本">
-<span id="clip_container"><span id="clip_button"><strong>复制</strong></span></span>
+<body >
 
-<!--
-<ul class="list-group">
-    <li class="list-group-item" align="center"><strong>注册成功!</strong></li>
-    <li class="list-group-item">
-        <?php
-        echo "请点击复制链接键将目标问卷分享到目标微信群，点击\"进入\"开始您的管理界面"."<br>";
-        echo "<br>". $userRegNewUrlOAuth;
-        echo "<button class=\"btn btn-large btn-block\" type=\"button\">复制链接</button>"."<br>";
-        ?>
-    </li>
-    <li class="list-group-item">
 
-    </li>
-</ul>
--->
+<form class="form-horizontal" role="form" method="get" action="../reg/parentRegWithStudent.php">
+    <div class="form-group">
+        <div class="col-sm-12" align="center">
+            <br><pre><h1>注册成功</h1></pre>
+            <br><br><h3>劳烦手动复制链接<br>将目标问卷分享到目标微信群</h3>
+
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-12">
+            <?php echo "<pre>";print_r($userRegNewUrlOAuth)  ; ?>
+        </div>
+    </div>
+
+
+    <div class="form-group" align="center">
+        <br>点击"进入"开始您的管理界面<br>
+            <button class="btn btn-large btn-block" type="button" onclick="window.location.href='../pages/homepage.php'">进入应用</button>
+
+    </div>
+</form>
 </body>
