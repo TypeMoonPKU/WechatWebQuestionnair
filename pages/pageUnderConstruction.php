72a131cb750517974ff920e3d5835976a9b4b17e
archimekai <bliewhale@163.com>
2016-04-24 23:34:34 +0800

    增加了正在施工页

---
+++ pages/pageUnderConstruction.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>正在开发中</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, charset=UTF-8">
+    <link href="./reference/bootstrap.min.css" rel="stylesheet">
+    <script src="./reference/jquery.min.js"></script>
+    <script src="./reference/bootstrap.min.js"></script>
+    <script src="./reference/bootstrap-theme.min.css"></script>
+</head>
+<body>
+<div class="container-fluid" id="LG">
+    <div class="row-fluid">
+        <div class="span12">
+            <div class="page-header">
+                <h1 align = "center">
+                    此功能正在开发中
+                </h1>
+            </div>
+        </div>
+    </div>
+    <div class="row-fluid">
+        <div class="span12">
+            <h2 contenteditable="true"><span style="font-size: 14px; line-height: 20px;">您可以资助开发者以加速开发进程<br><br></span></h2>
+        </div>
+        <img src="./pic/sun.jpg" width="33%" height=auto  />
+        <img src="./pic/chen.jpg" width="30%" height=auto  />
+        <img src="./pic/wen.jpg" width="33%" height=auto  />
+    </div>
+</div>
+
+</body>
+</html>
