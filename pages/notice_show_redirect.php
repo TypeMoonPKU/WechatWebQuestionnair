a7d9194427b39ea97d55c38a4f79e9838ffd9d25
archimekai <wenkai123111@126.com>
2016-05-08 16:42:30 +0800

    addRedirect

---
+++ pages/notice_show_redirect.php
@@ -0,0 +1,19 @@
+<?php
+require_once "../util/setHeaderToUTF8.php" // 通过使用
+?>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Archimekai
+ * Date: 5/8/2016
+ * Time: 15:58
+ */
+// 用于协助跳转
+// 用途：此链接是老师分享到朋友圈或者微信群的链接，区分老师和家长，如果没有openid的话应该跳转
+// 需要从来访的链接来分辨
+
+?>
+
+
+
---
+++ util/setHeaderToUTF8.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Archimekai
+ * Date: 5/8/2016
+ * Time: 16:23
+ */
+//此行代码用于避免iPhone上出现的乱码问题
+header("Content-type: text/html; charset=utf-8");
+?>
